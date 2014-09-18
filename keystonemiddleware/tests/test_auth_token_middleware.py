0bbe2e7f925ce307b88d067dc6beb1629f3347ab
Brant Knudson <bknudson@us.ibm.com>
2014-09-17 19:01:14 -0500

    Remove unused iso8601
    AuthProtocol doesn't have an _iso8601, so the test shouldn't set it.
    
    Change-Id: Ia4773be0fe9e17b96700e6aa56f663e706af94a8

--- keystonemiddleware/tests/test_auth_token_middleware.py
+++ keystonemiddleware/tests/test_auth_token_middleware.py
@@ -25,7 +25,6 @@ import uuid
 
 import fixtures
 import httpretty
-import iso8601
 from keystoneclient import access
 from keystoneclient.common import cms
 from keystoneclient import exceptions
@@ -268,7 +267,6 @@ class BaseAuthTokenMiddlewareTest(testtools.TestCase):
 
         self.middleware = auth_token.AuthProtocol(
             self.fake_app(self.expected_env), self.conf)
-        self.middleware._iso8601 = iso8601
 
         with tempfile.NamedTemporaryFile(dir=self.middleware._signing_dirname,
                                          delete=False) as f:
