70a9754ae68c3eb4d5645c76f3e61052486e9054
Brant Knudson <bknudson@us.ibm.com>
2016-01-22 09:50:06 -0600

    Revert "Disable memory caching of tokens"
    This reverts commit f27d7f776e8556d976f75d07c99373455106de52.
    
    This change broke the gate due to causing timeouts. The
    functionality needs to go through the normal deprecation before
    being removed.
    
    Change-Id: I4ab07f6e2bd5bd084bd16707126728929a4ba0f7

--- keystonemiddleware/tests/unit/utils.py
+++ keystonemiddleware/tests/unit/utils.py
@@ -17,7 +17,6 @@ import warnings
 
 import fixtures
 import mock
-from oslo_utils import timeutils
 import oslotest.base as oslotest
 import requests
 import uuid
@@ -151,53 +150,3 @@ class NoModuleFinder(object):
     def find_module(self, fullname, path):
         if fullname == self.module or fullname.startswith(self.module + '.'):
             raise ImportError
-
-
-class FakeMemcache(object):
-    """Replicates a tiny subset of memcached client interface."""
-
-    def __init__(self, *args, **kwargs):
-        """Ignores the passed in args."""
-        self.cache = {}
-
-    def get(self, key):
-        """Retrieves the value for a key or None.
-
-        This expunges expired keys during each get.
-        """
-
-        now = timeutils.utcnow_ts()
-        for k in list(self.cache):
-            (timeout, _value) = self.cache[k]
-            if timeout and now >= timeout:
-                del self.cache[k]
-
-        return self.cache.get(key, (0, None))[1]
-
-    def set(self, key, value, time=0, min_compress_len=0):
-        """Sets the value for a key."""
-        timeout = 0
-        if time != 0:
-            timeout = timeutils.utcnow_ts() + time
-        self.cache[key] = (timeout, value)
-        return True
-
-    def add(self, key, value, time=0, min_compress_len=0):
-        """Sets the value for a key if it doesn't exist."""
-        if self.get(key) is not None:
-            return False
-        return self.set(key, value, time, min_compress_len)
-
-    def incr(self, key, delta=1):
-        """Increments the value for a key."""
-        value = self.get(key)
-        if value is None:
-            return None
-        new_value = int(value) + delta
-        self.cache[key] = (self.cache[key][0], str(new_value))
-        return new_value
-
-    def delete(self, key, time=0):
-        """Deletes the value associated with a key."""
-        if key in self.cache:
-            del self.cache[key]
